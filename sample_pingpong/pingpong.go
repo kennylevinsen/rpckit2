c4a2eb1ce70d51014bed180fa6db61a052676607
Kenny Levinsen <w@kl.wtf>
2018-10-08 17:13:12 +0200

    Update pingpong example

--- sample_pingpong/pingpong.go
+++ sample_pingpong/pingpong.go
@@ -21,44 +21,66 @@ func main() {
 				panic(err)
 			}
 
-			connection, err := NewRPCConnection(conn, RPCTimeout(5 * time.Minute))
-			if err != nil {
-				panic(err)
-			}
-			RegisterPingpongHandler(connection, &server{})
-			RegisterEchoHandler(connection, &echoServer{})
-
-			connection.Start()
+			NewRPCBuilder().
+				Conn(conn).
+				Server(
+					PingpongHandler(&server{}),
+					EchoHandler(&echoServer{}),
+				).
+				ConnectHook(func(c *RPCConnection) error {
+					fmt.Printf("Server: user connected\n")
+					time.Sleep(5 * time.Second)
+					conn.Close()
+					return nil
+				}).
+				DisconnectHook(func(c *RPCConnection, err RPCError) {
+					fmt.Printf("Server: user disconnected\n")
+				}).
+				Build()
 		}
 	}()
 
-	// dial it
-	conn, err := net.Dial("tcp", "127.0.0.1:9999")
-	if err != nil {
-		panic(err)
-	}
-	connection, err := NewRPCConnection(conn,
-		RPCTimeout(5 * time.Minute),
-		RPCKeepAlive(5 * time.Second),
-		RPCDynamicKeepAlive(true))
-	if err != nil {
-		panic(err)
-	}
-	serverClient := NewPingpongClient(connection)
-	echoClient := NewEchoClient(connection)
-
-	connection.Start()
-
 	ctx := context.Background()
 
-	success, err := serverClient.Authenticate(ctx, "batman", "robin")
-	if err != nil || success == false {
-		panic(fmt.Sprintf("bad return values: %v, %v", success, err))
-	}
+	ready := make(chan struct{}, 0)
+
+	c := NewRPCBuilder().
+		Dialer(func() (net.Conn, error) {
+			return net.Dial("tcp", "127.0.0.1:9999")
+		}).
+		ConnectHook(func(c *RPCConnection) error {
+			fmt.Printf("Client: connected to server\n")
+			success, err := NewPingpongClient(c).Authenticate(ctx, "batman", "robin")
+			if err != nil {
+				panic(fmt.Sprintf("bad return values: %v, %v", success, err))
+				return err
+			}
+			if success == false {
+				panic(fmt.Sprintf("bad return values: %v, %v", success, err))
+			}
+
+			select {
+			case <-ready:
+			default:
+				close(ready)
+			}
+			return nil
+		}).
+		DisconnectHook(func(c *RPCConnection, err RPCError) {
+			fmt.Printf("Client: disconnected from server\n")
+		}).
+		Build()
+
+	<-ready
+
+	serverClient := NewPingpongClient(c)
+	echoClient := NewEchoClient(c)
+
 	greeting, err := serverClient.PingWithReply(ctx, "Oliver")
 	if err != nil {
 		panic(err)
 	}
+	// time.Sleep(10 * time.Second)
 
 	greeting2, err := echoClient.Echo(ctx, "weee", []string{"Hello", ", ", "World", "!"}, map[string]int64{
 		"five": 5,
@@ -69,6 +91,8 @@ func main() {
 		panic(err)
 	}
 
+	// time.Sleep(10 * time.Second)
+
 	_, err = serverClient.TestMethod(ctx, "hello", true, 1234, 654, 3.454, 3.14)
 	if err != nil {
 		panic(err)
@@ -76,7 +100,10 @@ func main() {
 	fmt.Println("greeting:", greeting)
 	fmt.Println("greeting2:", greeting2)
 	fmt.Println("done")
-	time.Sleep(time.Millisecond * 200)
+
+	c.Close()
+
+	time.Sleep(2 * time.Second)
 }
 
 type server struct {
