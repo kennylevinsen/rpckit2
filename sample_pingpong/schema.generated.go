d820dee41a7304dfd17851a2efae2840f8303126
Kenny Levinsen <kl@kl.wtf>
2019-06-18 14:36:23 +0200

    Prettify things

--- sample_pingpong/schema.generated.go
+++ sample_pingpong/schema.generated.go
@@ -4,8 +4,10 @@ import "context"
 
 // Echo thing
 type EchoThing struct {
+
 	// WAAAAH
 	Wee string `json:"wee"`
+
 	// Woo describes the woo factor
 	Woo string `json:"woo"`
 }
@@ -14,8 +16,10 @@ type EchoThing struct {
 type PingpongProtocol interface {
 	// Authenticate using username and password
 	Authenticate(ctx context.Context, reqUsername string, reqPassword string) (respSuccess bool, err error)
+
 	// PingWithReply replies with a greeting based on the provided name
 	PingWithReply(ctx context.Context, reqName string) (respGreeting string, err error)
+
 	// TestMethod is a simple type test
 	TestMethod(ctx context.Context, reqString string, reqBool bool, reqInt64 int64, reqInt int64, reqFloat float32, reqDouble float64) (respSuccess bool, err error)
 }
@@ -24,8 +28,10 @@ type PingpongProtocol interface {
 type EchoProtocol interface {
 	// Echo is yet another type test
 	Echo(ctx context.Context, reqInput string, reqNames []string, reqValues map[string]map[string]int64, reqSomething EchoThing) (respOutput string, err error)
+
 	// Ping is a simple no-input test
 	Ping(ctx context.Context) (respOutput string, err error)
+
 	// ByteTest is a byte test
 	ByteTest(ctx context.Context, reqInput []byte) (respOutput []byte, err error)
 }
